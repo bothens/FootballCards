33da4b66e59c1e8ac39754f5b78fb8b70f95a49b
NiklasRoos91 <niklas_roos@live.se>
2026-01-22 17:42:56 +0100

    added validator for IssueCard, PurchaseCard and SellCard

---
+++ backend/Application_Layer/Features/Card/Commands/Issue/IssueCardValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application_Layer.Features.Cards.Commands.Issue;
+using FluentValidation;
+
+namespace Application_Layer.Features.Card.Commands.Issue
+{
+    public sealed class IssueCardValidator : AbstractValidator<IssueCardCommand>
+    {
+        public IssueCardValidator()
+        {
+            RuleFor(x => x.Card.PlayerId)
+                .NotEmpty().WithMessage("PlayerId is required");
+
+            RuleFor(x => x.Card.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+
+            RuleFor(x => x.Card.CardType)
+                .MaximumLength(20).WithMessage("CardType cannot exceed 20 characters");
+        }
+    }
+}
