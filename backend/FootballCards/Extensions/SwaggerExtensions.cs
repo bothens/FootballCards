be6e1b2b65669aa10452bbb030839ca12ae943fb
Alexander Bothen <alexanderbothen@icloud.com>
2026-01-25 13:14:41 +0100

    update backend delen

--- backend/FootballCards/Extensions/SwaggerExtensions.cs
+++ backend/FootballCards/Extensions/SwaggerExtensions.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.OpenApi.Models;
-using Microsoft.Extensions.DependencyInjection;
-
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace FootballCards.API.Extensions
 {
@@ -16,6 +15,31 @@ namespace FootballCards.API.Extensions
                 {
                     Title = "FootballCards API",
                     Version = "v1"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Skriv: Bearer {din_token}"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
                 });
             });
 
